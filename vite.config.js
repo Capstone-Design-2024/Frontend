38abb502747fac2b18f77bb8edcd4c8d68f165b8
반영환 <lopahn2@gmail.com>
2024-06-22 21:05:55 +0900

    feat: fix nginx file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     include: ["redux-thunk"],
   },
 
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: "./",
 });
